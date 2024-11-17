a454d405981549fd270c0d5983b0a0277a7e9a62
Hinata Kaga (samon) <61647657+kakuteki@users.noreply.github.com>
2024-11-17 12:55:46 +0900

    ubutnu ver

--- serial_utils/ipc.py
+++ serial_utils/ipc.py
@@ -1,35 +1,20 @@
-# win32Pipe
-'''
-https://stackoverflow.com/questions/48542644/python-and-windows-named-pipes
-'''
-import struct, time
-import win32pipe, win32file, win32con, pywintypes
+import struct, time, os, sys
 
 class NamedPipe:
     def __init__(self, right_hand=True):
         if right_hand:
-            # OpenGloves /named-pipe-communication-manager/src/DeviceProvider.cpp#L77
-            self.pipename = r'\\.\\pipe\\vrapplication\\input\\right'
+            self.pipename = '/tmp/vrapplication_input_right'
         else:
-            self.pipename = r'\\.\\pipe\\vrapplication\\input\\left'
+            self.pipename = '/tmp/vrapplication_input_left'
         self.fingers = [False]*5
         self.joys = [0.0, 0.0]
         self.buttons = [False]*8
 
+        # Create named pipe if it doesn't exist
+        if not os.path.exists(self.pipename):
+            os.mkfifo(self.pipename)
+
     def encode(self, flexion, joys=None, bools=None):
-        ''' Struct format is from: https://github.com/LucidVR/opengloves-driver/.../EncodingManager.h#L17
-      const std::array<float, 5> flexion; // Between 0 and 1
-      const float joyX;     // Between -1 and 1
-      const float joyY;
-      const bool joyButton; // 0
-      const bool trgButton; // 1
-      const bool aButton;   // 2
-      const bool bButton;   // 3
-      const bool grab;      // 4
-      const bool pinch;     // 5
-      const bool menu;      // 6 
-      const bool calibrate; // 7
-        '''
         if joys is not None:
             print(joys)
             joyX = joys[0]
@@ -41,7 +26,6 @@ class NamedPipe:
         if bools is None:
             bools = [False, False, False, False, False, False, False, False] 
 
-        # https://tuttlem.github.io/2016/04/06/packing-data-with-python.html
         pack_obj = struct.pack('@5f', flexion[0], flexion[1], flexion[2], flexion[3], flexion[4])
         joys = struct.pack('@2f', joyX, joyY)
         pack_bools = struct.pack('@8?', *bools)
@@ -51,21 +35,10 @@ class NamedPipe:
     def send(self, fingers, joys=None, bools=None):
         encoded = self.encode(fingers, joys, bools)
         try:
-            # https://github.com/LucidVR/opengloves-driver/blob/develop/overlay/main.cpp#L128
-            open_mode = win32con.GENERIC_READ | win32con.GENERIC_WRITE
-
-            pipe = win32file.CreateFile(self.pipename,
-                                             open_mode,
-                                             0, # no sharing
-                                             None, # default security
-                                             win32con.OPEN_EXISTING,
-                                             0, # win32con.FILE_FLAG_OVERLAPPED,
-                                             None)
-            win32file.WriteFile(pipe, encoded)
-            win32file.CloseHandle(pipe)
-        except pywintypes.error:
-            print("Pipe busy")
-
+            with open(self.pipename, 'wb') as pipe:
+                pipe.write(encoded)
+        except OSError as e:
+            print(f"Pipe error: {e}")
 
 if __name__ == "__main__":
     ipc_right = NamedPipe(right_hand=True)
@@ -87,5 +60,4 @@ if __name__ == "__main__":
                             print(f"Wrote {fingers} to IPC")
     except KeyboardInterrupt:
         print("Quitting")
-        
         quit()
